9c27753149050781f1cd860581d4cabba31e0de7
Vasilii <cojocari.v88@gmail.com>
2019-07-11 13:53:11 +0300

    add cicle for

--- src/Zmeika/Snake.java
+++ src/Zmeika/Snake.java
@@ -1,7 +1,16 @@
 package Zmeika;
 
+import java.util.Random;
+
 public class Snake {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] numbers = new int[10];
+        Random rand = new Random();
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rand.nextInt(100) + 1;
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
     }
 }
