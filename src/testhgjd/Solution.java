3c607b9eefd52bc40695202cc810926e0acaa9e1
Vasilii <cojocari.v88@gmail.com>
2019-07-16 11:02:29 +0300

    add

--- src/testhgjd/Solution.java
+++ src/testhgjd/Solution.java
@@ -2,8 +2,26 @@ package testhgjd;
 
 
 public class Solution {
+    private static String name;
+
+    Solution(String name) {
+        this.name = name;
+    }
+
+    private static String getName() {
+        return name;
+    }
+
+    private void sout() {
+        new Solution("The Darkside Hacker") {
+            void printName() {
+                System.out.println(getName());
+            }
+        }.printName();
+    }
+
     public static void main(String[] args) {
-
-
+        new Solution("Риша").sout();
     }
 }
+
